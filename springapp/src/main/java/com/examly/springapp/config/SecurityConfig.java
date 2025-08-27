9edefa650fbdde866812f98089b89cdb69e7839d
Elakiyaa21 <elakiyaaramesh21@gmail.com>
2025-08-27 19:40:30 +0530

    deploy props

--- springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
@@ -70,7 +70,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource(@Value("${app.cors.allowed-origins:http://localhost:8081, https://e-wallet-management-system-flax.vercel.app}") String allowedOrigin) {
+    CorsConfigurationSource corsConfigurationSource(
+                    @Value("${app.cors.allowed-origins}") String allowedOrigin) {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(allowedOrigin));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
